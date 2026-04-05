62f55288dbf357d88ad3d1d6c0842555b5650f65
nobby <mr_nobby@mail.ru>
2026-04-05 18:12:07 +0300

    Решение задания № 3

---
+++ task_3.py
@@ -0,0 +1,23 @@
+print("Начался восьмичасовой рабочий день. ")
+the_bell = 0 # Звонк от Жены
+working_hours = 8 # Рабочие  часы
+max_tasks = 0 # Максимальное количество задач
+
+while working_hours != 0:
+
+   tasks = int (input("Сколько задач решит Максим ?"))
+   max_tasks += tasks
+   working_hours -= 1
+
+   if the_bell == 0:
+       the_bell = int(input("Звонит жена. Взять трубку?  (1 — да, 0 — нет): "))
+   else:
+       the_bell = 1
+
+   if working_hours == 0:
+       print("Конец Рабочего Дня !!! \nМаксим Решил задач ",max_tasks)
+       if the_bell == 1:
+           print("Звонила Жена и попросила зайти в магазин.")
+
+
+
